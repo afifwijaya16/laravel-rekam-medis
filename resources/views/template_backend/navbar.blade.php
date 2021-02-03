740ed8c6635c494bfbc933b8c84bf7f1f014c49a
afifwijaya16 <afif0071997@gmail.com>
2021-02-03 14:21:54 +0700

    make it better

--- resources/views/template_backend/navbar.blade.php
+++ resources/views/template_backend/navbar.blade.php
@@ -5,10 +5,7 @@
             <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
         </li>
         <li class="nav-item d-none d-sm-inline-block">
-            <a href="" class="nav-link">Home</a>
-        </li>
-        <li class="nav-item d-none d-sm-inline-block">
-            <a href="{{ url('/') }}" class="nav-link">Halaman Berita</a>
+            <a href="{{ url('/home') }}" class="nav-link">Home</a>
         </li>
     </ul>
     <ul class="navbar-nav ml-auto">
